9b29e28a21610441626a27da65bb7a3d76889c25
OmriTafla <omritafla@gmail.com>
2025-03-05 12:47:55 +0200

    .

--- Final Project/Final Project/obj/Debug/net9.0/Final Project.AssemblyInfo.cs
+++ Final Project/Final Project/obj/Debug/net9.0/Final Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2065ab530240582baf8cc931147c7a2e4896c4d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cf4b44aa5095f0fccd76672f9ca3f6d00f189a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
